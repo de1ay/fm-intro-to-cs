6bf5be1e92619280f35aae746eb3cb1fd5d75f9f
Nikita Ivanov <github@de1ay.me>
2024-11-20 23:50:02 +0300

    format go files

--- go/src/iterative_sorts/insertion_sort.go
+++ go/src/iterative_sorts/insertion_sort.go
@@ -1,18 +1,17 @@
 package iterativesorts
 
 func InsertionSort(arr []int) {
-  for i := 1; i < len(arr); i++ {
-    numberToInsert := arr[i]
-    j := i
+	for i := 1; i < len(arr); i++ {
+		numberToInsert := arr[i]
+		j := i
 
-    for ; j > 0; j-- {
-      if numberToInsert >= arr[j-1] {
-        break
-      }
-      arr[j] = arr[j-1]
-    }
+		for ; j > 0; j-- {
+			if numberToInsert >= arr[j-1] {
+				break
+			}
+			arr[j] = arr[j-1]
+		}
 
-    arr[j] = numberToInsert
-  }
+		arr[j] = numberToInsert
+	}
 }
-
