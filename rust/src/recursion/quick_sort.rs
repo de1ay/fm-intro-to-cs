3b5d90626d1a49ac29f571697c1d63bf6333bfb2
Nikita Ivanov <github@de1ay.me>
2024-11-21 19:06:56 +0300

    format quick sort in rust

--- rust/src/recursion/quick_sort.rs
+++ rust/src/recursion/quick_sort.rs
@@ -1,19 +1,21 @@
 pub fn quick_sort(arr: Vec<i32>) -> Vec<i32> {
-    if arr.len() <= 1 { return arr; }
+    if arr.len() <= 1 {
+        return arr;
+    }
 
-    let pivot = arr[arr.len()-1];
+    let pivot = arr[arr.len() - 1];
 
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
 
-    arr[..arr.len()-1].iter().for_each(|&num| {
+    arr[..arr.len() - 1].iter().for_each(|&num| {
         if num <= pivot {
             left.push(num);
         } else {
             right.push(num);
         }
     });
-    
+
     left = quick_sort(left);
     right = quick_sort(right);
 
